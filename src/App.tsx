695ab19db4c02226328af801a28a0b15a15c129e
Erick Namukolo <erickmndev@gmail.com>
2025-02-15 22:35:21 +0200

    added functionality to set the description and quantity state

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Item from './models/item';
 const initialItems: Item[] = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 2, description: 'Charger', quantity: 2, packed: true },
+  { id: 3, description: 'Charger', quantity: 2, packed: true },
 ];
 
 export default function App() {
--- src/componets/form.tsx
+++ src/componets/form.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form() {
+  const [description, setDescription] = useState<string | null>(null);
+  const [quantity, setQuantity] = useState<number>(1);
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    if (description == null) return;
+
+    console.log(description);
+  }
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        onChange={(e) => setQuantity(Number.parseInt(e.target.value))}
+        value={quantity}
+      >
         {Array.from({ length: 30 }, (_, i) => (
           <option value={i + 1} key={i}>
             {i + 1}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...'></input>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description ?? ''}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>Add</button>
     </form>
   );
